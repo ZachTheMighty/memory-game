b9b8777aa7c1a1aa1a0c46390c43af288dbf04aa
ZachTheMighty <promoted66@gmail.com>
2026-07-31 16:33:52 +0300

    add: cards box shadow on hover

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import img from "/home/klux/downloads/vagabond.jpg";
 
 export default function Card() {
   return (
-    <div className="bg-slate-700 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer">
+    <div className="bg-slate-800 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer hover:shadow-[0_0px_10px_0px_rgba(255,255,255,0.5)]">
       <img className="h-[80%] object-cover rounded-md" src={img} />
       <div className="h-[20%] flex justify-center items-center text-2xl font-semibold">
         text
