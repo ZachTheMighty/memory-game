c053b2eee32a943ff0a7c6b1cf9901f3e37c59d9
ZachTheMighty <promoted66@gmail.com>
2026-07-31 15:36:34 +0300

    add: more styles to card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,12 @@
+import img from "/home/klux/downloads/vagabond.jpg";
+
 export default function Card() {
   return (
-    <div className="bg-sky-300 h-80">
-      <div className="h-[80%] bg-gray-800"></div>
-      <div className="h-[20%]">text</div>
+    <div className="bg-slate-700 h-80 p-2 rounded-md">
+      <img className="h-[80%] object-cover rounded-md" src={img} />
+      <div className="h-[20%] flex justify-center items-center text-2xl font-semibold">
+        text
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "../index.css";
 
 const root = document.getElementById("root");
-root.className = "min-h-screen bg-gray-900 text-white p-8";
+root.className = "min-h-screen bg-slate-900 text-white p-8";
 
 createRoot(root).render(
   <StrictMode>
