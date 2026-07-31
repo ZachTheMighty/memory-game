1f05f845013153e1353f1020be541aa65b71dc6c
ZachTheMighty <promoted66@gmail.com>
2026-07-31 16:23:40 +0300

    add: hover styles on cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import img from "/home/klux/downloads/vagabond.jpg";
 
 export default function Card() {
   return (
-    <div className="bg-slate-700 h-80 p-2 rounded-md">
+    <div className="bg-slate-700 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer">
       <img className="h-[80%] object-cover rounded-md" src={img} />
       <div className="h-[20%] flex justify-center items-center text-2xl font-semibold">
         text
