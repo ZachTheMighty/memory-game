8278d887ce7b02dd6fb9a717fd9829a9c43b200f
ZachTheMighty <promoted66@gmail.com>
2026-08-01 08:11:46 +0300

    add: static scores

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ export default function App() {
   ).sort(() => Math.random() - 0.5);
 
   return (
-    <div className="min-h-screen grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-16">
-      {gifs.map((gif, index) => (
-        <Card gif={gif} key={index} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col mb-4 text-xl sm:text-2xl font-semibold">
+        <div>Score: 0</div>
+        <div>Best: 0</div>
+      </div>
+      <div className="min-h-screen grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-16">
+        {gifs.map((gif, index) => (
+          <Card gif={gif} key={index} />
+        ))}
+      </div>
+    </>
   );
 }
