99ff434d96fcb7f5d1f50b0bf49ddb45f195269f
ZachTheMighty <promoted66@gmail.com>
2026-08-01 07:05:47 +0300

    refactor: used mockup api instead of actual one because giphy sucks ass

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,23 +2,32 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Card() {
-  const [img, setImg] = useState(null);
+  const [gifs, setGifs] = useState(
+    Object.values(import.meta.glob("../assets/*", { eager: true })),
+  );
+  const [gif, setGif] = useState(null);
+
+  function fetchData() {
+    const random = Math.floor(Math.random() * 11);
+    return Promise.resolve({
+      data: { images: { original: { url: gifs[random] } } },
+    });
+  }
 
   useEffect(() => {
-    fetch(
-      "https://api.giphy.com/v1/gifs/random?api_key=OTDZKDx5zFC6L8ExDOOKwsWJAI4spszj&tag=simpsons",
-    )
-      .then((response) => response.json())
-      .then((data) =>
-        setImg({ url: data.data.images.original.url, title: data.data.title }),
-      );
-  });
+    fetchData().then((data) =>
+      setGif({
+        url: data.data.images.original.url.default,
+        title: data.data.title,
+      }),
+    );
+  }, []);
 
   return (
     <div className="bg-slate-800 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer hover:shadow-[0_0px_10px_0px_rgba(255,255,255,0.5)]">
-      <img className="h-[80%] object-cover rounded-md" src={img?.url} />
+      <img className="h-[80%] object-cover rounded-md" src={gif?.url} />
       <div className="h-[20%] flex justify-center items-center text-2xl font-semibold">
-        {img?.title}
+        {gif?.title}
       </div>
     </div>
   );
