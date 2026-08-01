0bb522d1d90640ddba55084c5d9cee8458678229
ZachTheMighty <promoted66@gmail.com>
2026-08-01 07:06:30 +0300

    fix: width of gif

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -25,7 +25,7 @@ export default function Card() {
 
   return (
     <div className="bg-slate-800 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer hover:shadow-[0_0px_10px_0px_rgba(255,255,255,0.5)]">
-      <img className="h-[80%] object-cover rounded-md" src={gif?.url} />
+      <img className="w-full h-[80%] object-cover rounded-md" src={gif?.url} />
       <div className="h-[20%] flex justify-center items-center text-2xl font-semibold">
         {gif?.title}
       </div>
