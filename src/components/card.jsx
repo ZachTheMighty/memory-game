8662ccd114d1f65c513ae665070b077046959e35
ZachTheMighty <promoted66@gmail.com>
2026-08-01 10:45:02 +0300

    add: logic for incrementing score when clicking a card that's not been clicked before

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import getTitle from "../utils/get_title.js";
 
-export default function Card({ gif, gifs, setGifs, shuffle }) {
+export default function Card({ gif, gifs, setGifs, shuffle, setScore }) {
+  const [clicked, setClicked] = useState(false);
   return (
     <div
       onClick={() => {
+        !clicked && setScore((prevScore) => prevScore + 1);
+        setClicked(true);
         setGifs(shuffle(gifs));
       }}
       className="bg-slate-800 h-80 p-2 rounded-md hover:-translate-y-1 trasnition-translate-y duration-100 hover:cursor-pointer hover:shadow-[0_0px_10px_0px_rgba(255,255,255,0.5)]"
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -6,11 +6,12 @@ export default function Game() {
   const [gifs, setGifs] = useState(
     shuffle(Object.values(import.meta.glob("../assets/*", { eager: true }))),
   );
+  const [score, setScore] = useState(0);
 
   return (
     <>
       <div className="flex flex-col mb-4 text-xl sm:text-2xl font-semibold">
-        <div>Score: 0</div>
+        <div>Score: {score}</div>
         <div>Best: 0</div>
       </div>
       <div className="min-h-screen grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-16">
@@ -20,6 +21,7 @@ export default function Game() {
             key={gif.default}
             gifs={gifs}
             setGifs={setGifs}
+            setScore={setScore}
             shuffle={shuffle}
           />
         ))}
