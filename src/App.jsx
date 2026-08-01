ed4cd532e2e66103ef80ff7199980adcd7398861
ZachTheMighty <promoted66@gmail.com>
2026-08-01 04:39:26 +0300

    refactor: used loop to render multiple components instead of hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,9 @@ import Card from "./components/card.jsx";
 export default function App() {
   return (
     <div className="min-h-screen grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-16">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {Array.from({ length: 12 }).map((_, index) => (
+        <Card key={index} />
+      ))}
     </div>
   );
 }
